45ed8d781d0a1620d862c1cb24be62439e92bab2
monqcq <takuyaaaan23@gmail.com>
2021-07-08 14:54:32 +0900

    [add]天気による行動の変化をjavaで表す

---
+++ src/Code3_1.java
@@ -0,0 +1,12 @@
+
+public class Code3_1 {
+	public static void main(String[] args) {
+		boolean tenki = true;
+		if (tenki == true) {
+			System.out.println("選択に行きます");
+			System.out.println("散歩に行きます");
+		} else {
+			System.out.println("DVDを見ます");
+		}
+	}
+}
