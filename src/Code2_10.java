bf2dff19c4549237bf365650501a0d81b4a7a8ac
monqcq <takuyaaaan23@gmail.com>
2021-07-08 13:59:37 +0900

    [add]命令実行の文（画面に表示する）

---
+++ src/Code2_10.java
@@ -0,0 +1,9 @@
+
+public class Code2_10 {
+	public static void main(String[] args) {
+		String name = "すがわら";
+		String message;
+		message = name + "さん、こんにちは";
+		System.out.println(message);
+	}
+}
