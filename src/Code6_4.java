2e4140da0d841724cbd137f2dd91d648032e72db
monqcq <takuyaaaan23@gmail.com>
2021-07-10 13:03:49 +0900

    [add]正しく修正したCode6_3

--- src/Code6_4.java
+++ src/Code6_4.java
@@ -1,9 +1,9 @@
 
-public class Code6_3 {
+public class Code6_4 {
 	public static void main(String[] args) {
 		int a = 10; int b = 2;
-		int total = tasu(a, b);
-		int delta = hiku(a ,b);
+		int total = Code6_2.tasu(a, b);
+		int delta = Code6_2.hiku(a ,b);
 		System.out.println("足すと" + total + "、引くと" + delta);
 	}
 }
