0fc607a9e1210e18030e72ed98bfad34ed4c294c
monqcq <takuyaaaan23@gmail.com>
2021-07-10 10:31:09 +0900

    [add]配列の長さを調べる

--- src/Code4_2.java
+++ src/Code4_2.java
@@ -1,4 +1,7 @@
 
 public class Code4_2 {
-
+	public static void main(String[] args) {
+		int[] scores;
+		scores = new int[5];
+	}
 }
--- src/Code4_3.java
+++ src/Code4_3.java
@@ -1,4 +1,6 @@
 
 public class Code4_3 {
-
+	public static void main(String[] args) {
+		int scores[] = new int[5];
+	}
 }
