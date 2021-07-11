7de63c3b5dd567e8c13a8b625fd038ed70321bc3
monqcq <takuyaaaan23@gmail.com>
2021-07-11 20:40:02 +0900

    [add]コード８−１２　勇者インスタンスを生成して初期値を代入

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@ public class Main {
 	public static void main(String[] args) {
 		//1.勇者を作成
 		Hero h = new Hero();
+		//2.フィールドに初期値をセット
+		h.name = "ミナト";
+		h.hp = 100;
+		System.out.println("勇者" + h.name + "を生み出しました！");
 	}
 }
