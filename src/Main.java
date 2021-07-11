44cddd058a023c1979364200842b152e8cf7036c
monqcq <takuyaaaan23@gmail.com>
2021-07-11 20:43:09 +0900

    [add]コード８−１３　仮想世界に勇者を生み出すプログラム

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,10 @@ public class Main {
 		h.name = "ミナト";
 		h.hp = 100;
 		System.out.println("勇者" + h.name + "を生み出しました！");
+		//勇者のメソッドを呼び出してゆく
+		h.sit(5);
+		h.slip();
+		h.sit(25);
+		h.run();
 	}
 }
