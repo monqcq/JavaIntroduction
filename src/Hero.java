259995e301182b16586bc27f81261691c77eb08d
monqcq <takuyaaaan23@gmail.com>
2021-07-12 23:31:42 +0900

    [add]Sword型のフィールドを持つHero

--- src/Hero.java
+++ src/Hero.java
@@ -1,28 +1,30 @@
 
 public class Hero {
-	String name; //名前の宣言
-	int hp = 10; //HPの宣言
-	
+	String name; // 名前の宣言
+	int hp = 10; // HPの宣言
+	Sword sword;
+
 	public void sleep() {
 		this.hp = 100;
 		System.out.println(this.name + "は、眠って回復した！");
 	}
-	
+
 	public void sit(int sec) {
 		this.hp += sec;
 		System.out.println(this.name + "は、" + sec + "秒座った！");
 		System.out.println("HPが" + sec + "ポイント回復した");
 	}
-	
+
 	public void slip() {
 		this.hp -= 5;
 		System.out.println(this.name + "は、転んだ！");
 		System.out.println("5のダメージ！");
 	}
+
 	public void run() {
 		System.out.println(this.name + "は、逃げ出した！");
 		System.out.println("GAMEOVER");
 		System.out.println("最終HPは" + this.hp + "でした");
 	}
-	
+
 }
