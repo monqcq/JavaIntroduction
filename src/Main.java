1332d0fae3bbf7ebd9be94b052b19ee6feae3132
monqcq <takuyaaaan23@gmail.com>
2021-07-14 12:45:16 +0900

    [add]コード9-6 勇者と魔法使いを生み出す

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,17 @@
 
 public class Main {
 	public static void main(String[] args) {
-		Sword s = new Sword();
-		s.name = "炎の剣";
-		s.damage = 10;
-		Hero h = new Hero();
-		h.name = "ミナト";
-		h.hp = 100;
-		h.sword = s;
-		System.out.println("現在の武器は" + h.sword.name);
-
-		Matango m1 = new Matango();
-		m1.hp = 50;
-		m1.suffix = 'A';
-		Matango m2 = new Matango();
-		m2.hp = 48;
-		m2.suffix = 'B';
-
-		// 冒険の始まり
-		h.slip();
-		m1.run();
-		m2.run();
-		h.run();
+		Hero h1 = new Hero();
+		h1.name = "ミナト";
+		h1.hp = 100;
+		Hero h2 = new Hero();
+		h2.name = "アサカ";
+		h2.hp = 100;
+		Wizard w = new Wizard();
+		w.name = "スガワラ";
+		w.hp = 50;
+		w.heal(h1);
+		w.heal(h2);
+		w.heal(h2);
 	}
 }
