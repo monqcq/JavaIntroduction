4ba9ed2aafbd4f7d49aae10f327c4e769c47f014
monqcq <takuyaaaan23@gmail.com>
2021-07-15 17:31:30 +0900

    [add]コード10-1 戦うと逃げるしかできないHeroクラス

--- src/Hero.java
+++ src/Hero.java
@@ -1,35 +1,16 @@
 
 public class Hero {
-	String name; // 名前の宣言
-	int hp = 10; // HPの宣言
-	Sword sword;
-
-	public void sleep() {
-		this.hp = 100;
-		System.out.println(this.name + "は、眠って回復した！");
-	}
-
-	public void sit(int sec) {
-		this.hp += sec;
-		System.out.println(this.name + "は、" + sec + "秒座った！");
-		System.out.println("HPが" + sec + "ポイント回復した");
-	}
-
-	public void slip() {
-		this.hp -= 5;
-		System.out.println(this.name + "は、転んだ！");
-		System.out.println("5のダメージ！");
-	}
-
-	public void run() {
-		System.out.println(this.name + "は、逃げ出した！");
-		System.out.println("GAMEOVER");
-		System.out.println("最終HPは" + this.hp + "でした");
+	String name = "ミナト";
+	int hp = 100;
+	
+	public void attack(Matango m) {
+		System.out.println(this.name + "の攻撃！");
+		m.hp -= 5;
+		System.out.println("5ポイントのダメージを与えた！");
 	}
 	
-	public Hero(String name) {
-		this.hp = 100;
-		this.name = name;
+	public void run() {
+		System.out.println(this.name + "は逃げ出した！");
 	}
 
 }
