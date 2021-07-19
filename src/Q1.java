961665114cb5e0a4254d5e2bcb6955ada9a66308
monqcq <takuyaaaan23@gmail.com>
2021-07-19 21:36:36 +0900

    [add]RJSで出た復習３

--- src/Q1.java
+++ src/Q1.java
@@ -3,9 +3,11 @@ public class Q1 {
 	public static void main(String[] args) {
 		int num = 10;
 		int count = 0;
+		int add = 0;
 		while (num >= 0) {
 			if (num % 2 == 0) {
 				System.out.println(num);
+				add += num;
 				if (num != 0) {
 					count++;
 				}
@@ -13,5 +15,6 @@ public class Q1 {
 			num--;
 		}
 		System.out.println("偶数の個数は" + count);
+		System.out.println("偶数の合計値は" + add);
 	}
 }
---
+++ src/Q2.java
@@ -0,0 +1,14 @@
+
+public class Q2 {
+	public static void main(String[] args) {
+		int num = 1;
+		while(num <= 10) {
+			if(num != 10) {
+				System.out.print(num + ",");				
+			}else {
+				System.out.print(num);
+			}
+			num++;
+		}
+	}
+}
