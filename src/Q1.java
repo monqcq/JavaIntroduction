6ffbc8756d0dba15092d228263b3d489e9476842
monqcq <takuyaaaan23@gmail.com>
2021-07-19 21:07:49 +0900

    [add]RJCで出た復習

---
+++ src/Q1.java
@@ -0,0 +1,12 @@
+
+public class Q1 {
+	public static void main(String[] args) {
+		int num = 10;
+		while(num >= 0) {
+			if(num % 2 == 0) {
+				System.out.println(num);
+			}
+			num--;
+		}
+	}
+}
