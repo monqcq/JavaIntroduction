1973562e865ce28806e40cfbea4af61df405715c
monqcq <takuyaaaan23@gmail.com>
2021-07-22 17:06:40 +0900

    [add]Code13-4 hpフィールドをprivateにしたHeroクラス

--- src/capsule/Hero.java
+++ src/capsule/Hero.java
@@ -1,5 +1,23 @@
 package capsule;
 
 public class Hero {
-
+	int hp;
+	String name;
+	Sword sword;
+	
+	public void bye() {
+		System.out.println("勇者は別れを告げた");
+	}
+	public void die() {
+		System.out.println(this.name + "は死んでしまった！");
+		System.out.println("GAME OVERです。");
+	}
+	public void sleep() {
+		this.hp = 100;
+		System.out.println(this.name + "は眠って回復した");
+	}
+	public void attack(Matango m) {
+		System.out.println(this.name + "の攻撃！");
+		m.hp -= 5;
+	}
 }
