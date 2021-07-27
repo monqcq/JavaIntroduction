197f8742047d3a90a8dea6d6b938efe37df40d3e
monqcq <takuyaaaan23@gmail.com>
2021-07-27 19:58:25 +0900

    [add]クエスチョン5

---
+++ src/Q5.java
@@ -0,0 +1,14 @@
+
+public class Q5 {
+	public static void main(String[] args) {
+		int num = 0;
+		while (num <= 10) {
+			if (num % 2 == 0) {
+
+				System.out.println(num);
+			}
+			num++;
+
+		}
+	}
+}
