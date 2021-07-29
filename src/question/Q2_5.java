13007ea0073b3121a6f448b5377282e764f5b200
monqcq <takuyaaaan23@gmail.com>
2021-07-29 21:22:00 +0900

    [add]問題2-5

---
+++ src/question/Q2_5.java
@@ -0,0 +1,13 @@
+package question;
+
+import java.util.Scanner;
+
+public class Q2_5 {
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		System.out.print("整数値は：");
+		int x = stdIn.nextInt();
+
+		System.out.println(x + "と入力しましたね。");
+	}
+}
