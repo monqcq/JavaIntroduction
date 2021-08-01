7563f4c6d7944039667df1b1e6417c921d6f1712
monqcq <takuyaaaan23@gmail.com>
2021-08-01 15:13:21 +0900

    [add]問題2-11

---
+++ src/question/Q2_11.java
@@ -0,0 +1,16 @@
+package question;
+import java.util.Random;
+
+public class Q2_11 {
+	public static void main(String[] args) {
+		Random rand = new Random();
+		int n1 = 1 + rand.nextInt(9);
+		int n2 = -1 - rand.nextInt(9);
+		int n3 = 10 + rand.nextInt(90);
+		
+		System.out.println("3個の乱数を生成しました");
+		System.out.println("1桁の正の整数：" + n1);
+		System.out.println("1桁の負の整数：" + n2);
+		System.out.println("2桁の正の整数：" + n3);
+	}
+}
