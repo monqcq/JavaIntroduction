44019e6da9318fcd08c997ccb21f888344c38050
monqcq <takuyaaaan23@gmail.com>
2021-08-02 14:09:38 +0900

    [add]問題3-3

---
+++ src/question/Q3_3.java
@@ -0,0 +1,20 @@
+package question;
+
+import java.util.Scanner;
+
+public class Q3_3 {
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		System.out.print("変数A:");
+		int a = stdIn.nextInt();
+		System.out.print("変数B:");
+		int b = stdIn.nextInt();
+
+		if (a % b == 0) {
+			System.out.println("BはAの約数です。");
+		} else {
+			System.out.println("BはAの約数ではありません");
+		}
+
+	}
+}
