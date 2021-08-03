69be66030d9196d8ce8fe2e857ec2bcd3df2a4d4
monqcq <takuyaaaan23@gmail.com>
2021-08-03 16:46:27 +0900

    [add]問題3-10

---
+++ src/question/Q3_10.java
@@ -0,0 +1,24 @@
+package question;
+
+import java.util.Scanner;
+
+public class Q3_10 {
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		System.out.print("整数値：");
+		int n = stdIn.nextInt();
+
+		if (n > 0) {
+			if (n % 3 == 0) {
+				System.out.println("その値は3で割り切れます");
+			} else if (n % 3 == 1) {
+				System.out.println("その値を3で割った余りは1です");
+			} else {
+				System.out.println("その値を3で割った余りは2です");
+			}
+		} else {
+			System.out.println("正でない値が入力されました");
+		}
+	}
+	
+}
