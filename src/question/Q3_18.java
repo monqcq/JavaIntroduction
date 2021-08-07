d85eed6653c74eaff8d60acb73d6a59831c1ff30
monqcq <takuyaaaan23@gmail.com>
2021-08-07 20:20:14 +0900

    [add]問題3-18

---
+++ src/question/Q3_18.java
@@ -0,0 +1,23 @@
+package question;
+
+import java.util.Scanner;
+
+public class Q3_18 {
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+
+		System.out.print("変数a:");
+		int a = stdIn.nextInt();
+		System.out.print("変数b:");
+		int b = stdIn.nextInt();
+
+		if (a < b) {
+			int t = a;
+			a = b;
+			b = t;
+		}
+		System.out.println("a≧bとなるようにソートしました");
+		System.out.println("変数aは" + a + "です。");
+		System.out.println("変数bは" + b + "です。");
+	}
+}
