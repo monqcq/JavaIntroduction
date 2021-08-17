9d67e8523e6440f218fea2908af4480d2d257c7f
monqcq <takuyaaaan23@gmail.com>
2021-08-17 13:24:16 +0900

    [add]問題4-6

--- src/question/Q4_6.java
+++ src/question/Q4_6.java
@@ -1,5 +1,22 @@
 package question;
 
-public class Q4_6 {
+import java.util.Scanner;
 
+public class Q4_6 {
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+
+		System.out.println("カウントダウンします");
+		int x;
+		do {
+			System.out.print("正の整数値：");
+			x = stdIn.nextInt();
+		} while (x <= 0);
+
+		while (x >= 0) {
+			System.out.println(--x);
+		}
+
+		System.out.println("xの値は" + x + "になりました");
+	}
 }
