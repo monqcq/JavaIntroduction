3506b9473b41a8bdd05d5d51b8f7bef47f62a423
monqcq <takuyaaaan23@gmail.com>
2021-08-23 14:34:56 +0900

    [add]問題4-16

---
+++ src/question/Q4_16.java
@@ -0,0 +1,25 @@
+package question;
+
+import java.util.Scanner;
+
+public class Q4_16 {
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+
+		System.out.print("何cmから：");
+		int hMin = stdIn.nextInt();
+
+		System.out.print("何cmまで：");
+		int hMax = stdIn.nextInt();
+
+		System.out.print("何cmごと：");
+		int step = stdIn.nextInt();
+
+		System.out.println("身長　標準体重");
+		System.out.println("------------");
+
+		for (int i = hMin; i <= hMax; i += step) {
+			System.out.println(i + " " + 0.9 * (i - 100));
+		}
+	}
+}
