78695062ada3045738844fba36e5c8360618d5a8
monqcq <takuyaaaan23@gmail.com>
2021-09-05 21:18:46 +0900

    [add]問題4-26

---
+++ src/question/Q4_26.java
@@ -0,0 +1,27 @@
+package question;
+
+import java.util.Scanner;
+
+public class Q4_26 {
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		System.out.println("整数を加算します");
+		System.out.print("何個加算しますか：");
+		int n = stdIn.nextInt();
+
+		int sum = 0;
+		int i;
+		for (i = 0; i < n; i++) {
+			System.out.print("整数（０で終了）：");
+			int t = stdIn.nextInt();
+			if (t == 0) {
+				break;
+			}
+			sum += t;
+		}
+		System.out.println("合計は" + sum + "です。");
+		if (i != 0) {
+			System.out.println("平均は" + sum / i + "です。");
+		}
+	}
+}
