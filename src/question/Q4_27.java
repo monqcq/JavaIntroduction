44e594e9d522c103894a37901b38467bd9d87125
monqcq <takuyaaaan23@gmail.com>
2021-09-06 23:32:57 +0900

    [add]問題4-27

---
+++ src/question/Q4_27.java
@@ -0,0 +1,29 @@
+package question;
+
+import java.util.Scanner;
+
+public class Q4_27 {
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		System.out.println("整数を加算します");
+		System.out.print("何個加算しますか");
+		int n = stdIn.nextInt();
+
+		int sum = 0;
+		int i;
+		for (i = 0; i < n; i++) {
+			System.out.print("整数:");
+			int t = stdIn.nextInt();
+			if (sum + t > 1000) {
+				System.out.println("合計が１０００を超えました。");
+				System.out.println("最後の数値は無視します");
+				break;
+			}
+			sum += t;
+		}
+		System.out.println("合計は" + sum + "です");
+		if (i != 0) {
+			System.out.println("平均は" + sum / i + "です");
+		}
+	}
+}
